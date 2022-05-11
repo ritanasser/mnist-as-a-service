44bb0ad8a56c2fa67604b31082b1ec2acc0f40df
rita- <ritanas00@gmail.com>
2022-05-11 19:04:54 +0300

    jnk

--- package_demo/setup.py
+++ package_demo/setup.py
@@ -1,11 +1,10 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
-import register as register
-import upload as upload
 from setuptools import setup, find_packages
 import os
 from distutils.command.register import register as register_orig
 from distutils.command.upload import upload as upload_orig
+from setuptools import setup
 
 setup_args = dict(
     name='fantastic_ascii',
